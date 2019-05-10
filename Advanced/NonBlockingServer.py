ed3e08b76bea137a3217789e65c74a2e00a01828
David M. Golembiowski <dmgolembiowski@gmail.com>
2019-05-10 15:51:56 -0400

    Update NonBlockingServer.py

--- Advanced/NonBlockingServer.py
+++ Advanced/NonBlockingServer.py
@@ -19,11 +19,14 @@ def reactor(host='localhost', port=9600):
     s.bind((host, port))
     s.listen(5)
     #s.setblocking(0)          # Make asynchronous.  Never wait on a client socket.
-    """ I commented out `s.setblocking(0)`  
+    
+    """ 
+    I commented out `s.setblocking(0)`  
     because it's not immediately obvious to 
     everyone that it's equivalent to
     `s.setblocking(False)`
     """
+    
     s.setblocking(False) # Do not let the client socket block other activity
     sessions[s] = None
     print('Server up, running, and waiting for call on %s %s' % (host, port))
